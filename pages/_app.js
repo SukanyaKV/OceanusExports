b49697eeecea5ce577e2bfd245f54f429aafb4dc
SukanyaKV <kvsukanya95@gmail.com>
2022-04-28 14:00:15 +0530

    initial commit

---
+++ pages/_app.js
@@ -0,0 +1,11 @@
+import '../styles/globals.css'
+// import "./app.css"
+// import React from "react";
+// import axios from "axios";
+
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
